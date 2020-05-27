3abc8f85485cb23dba626233d4b948313a2cb0e1
Hua <dryadd44651@gmail.com>
2020-05-26 23:27:15 -0500

    update

--- src/Routes.js
+++ src/Routes.js
@@ -39,8 +39,8 @@ const measurer = [
 const products = [
     {id:0, title: '工業應用', sub:'Silicon按鍵',img:'/assets/imgs/industry/按鍵.jpg',content:'抗老化,耐磨,彈性佳'},
     {id:1, title: '工業應用', sub:'silicon O-Ring',img:'/assets/imgs/industry/o-ring.jpg',content:'耐酸鹼,耐溶劑,抗靜電'},
-    {id:2, title: '工業應用', sub:'silicon防水圈',img:'/assets/imgs/industry/water-proof.jpg',content:'耐酸鹼,耐溶劑,抗靜電'},
-    {id:3, title: '醫療科技', sub:'silicon醫療矽膠',img:'/assets/imgs/Medical/silicon.jpg',content:'無毒,無味,耐高溫'},
+    {id:2, title: '工業應用', sub:'silicon防水圈',img:'/assets/imgs/industry/water-proof.JPG',content:'耐酸鹼,耐溶劑,抗靜電'},
+    {id:3, title: '醫療科技', sub:'silicon醫療矽膠',img:'/assets/imgs/Medical/silicon.JPG',content:'無毒,無味,耐高溫'},
     {id:4, title: '生活科技', sub:'silicon醫療鍵盤',img:'/assets/imgs/life/鍵盤.jpg',content:'無毒,無味,防塵'},
     {id:5, title: '生活科技', sub:'silicon果凍套',img:'/assets/imgs/life/3C.jpg',content:'延展性佳,防水性佳,質地柔軟'},
     {id:6, title: '生活科技', sub:'silicon水上用品',img:'/assets/imgs/life/水上用品.jpg',content:'耐用,舒適,透光性佳'},
