9f7fd9c8950f361f9b2c573a6501bf8ef3fb6904
Hua <dryadd44651@gmail.com>
2020-05-26 22:24:58 -0500

    service photo update

--- src/Routes.js
+++ src/Routes.js
@@ -12,25 +12,27 @@ import NotFound from './components/404';
 
 //exact: using in parent route "/" "product"
 const equipment = [
-    {id:0,title: 'IR輸送式乾燥機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:1, title: 'LSR射出機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:2, title: '固態真空油壓機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:3, title: '裁切機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:4, title: '印刷台', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:5, title: '中型平面網印機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:6, title: '電動跑檯式退料網印機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:7, title: '混料機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:8, title: '烤箱', img:'/assets/imgs/logo.png',content:'XXX...'},
+    {id:0,title: 'IR輸送式乾燥機', img:'/assets/imgs/equipment/1.jpg',content:'XXX...'},
+    {id:1, title: 'LSR射出機', img:'/assets/imgs/equipment/2.jpg',content:'XXX...'},
+    {id:2, title: '固態真空油壓機', img:'/assets/imgs/equipment/3.jpg',content:'XXX...'},
+    {id:3, title: '裁切機', img:'/assets/imgs/equipment/4.jpg',content:'XXX...'},
+    {id:4, title: '印刷台', img:'/assets/imgs/equipment/5.jpg',content:'XXX...'},
+    {id:5, title: '中型平面網印機', img:'/assets/imgs/equipment/6.jpg',content:'XXX...'},
+    {id:6, title: '電動跑檯式退料網印機', img:'/assets/imgs/equipment/7.jpg',content:'XXX...'},
+    {id:7, title: '混料機', img:'/assets/imgs/equipment/8.jpg',content:'XXX...'},
+    {id:8, title: '烤箱', img:'/assets/imgs/equipment/9.jpg',content:'XXX...'},
 ]
 const measurer = [
-    {id:0, title: '壽命測試機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:1, title: '荷重曲線量測儀器', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:2, title: '放大檢視鏡', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:3, title: '耐摩擦試驗機', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:4, title: '游標卡尺', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:5, title: '硬度計', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:6, title: '厚薄規', img:'/assets/imgs/logo.png',content:'XXX...'},
-    {id:7, title: '厚薄規', img:'/assets/imgs/logo.png',content:'XXX...'},
+    {id:0, title: '手動荷重計', img:'/assets/imgs/measurer/1.jpg',content:'XXX...'},
+    {id:1, title: '2.5D量測儀', img:'/assets/imgs/measurer/2.jpg',content:'XXX...'},
+    {id:2, title: '壽命測試機', img:'/assets/imgs/measurer/3.jpg',content:'XXX...'},
+    {id:3, title: '荷重曲線量測儀器', img:'/assets/imgs/measurer/4.jpg',content:'XXX...'},
+    {id:4, title: '放大檢視鏡', img:'/assets/imgs/measurer/5.jpg',content:'XXX...'},
+    {id:5, title: '耐摩擦試驗機', img:'/assets/imgs/measurer/6.jpg',content:'XXX...'},
+    {id:6, title: '游標卡尺', img:'/assets/imgs/measurer/7.jpg',content:'XXX...'},
+    {id:7, title: '硬度計', img:'/assets/imgs/measurer/8.jpg',content:'XXX...'},
+    {id:8, title: '厚薄規', img:'/assets/imgs/measurer/9.jpg',content:'XXX...'},
+    {id:9, title: '厚薄規', img:'/assets/imgs/measurer/10.jpg',content:'XXX...'},
     
 ]
 
@@ -38,7 +40,7 @@ const products = [
     {id:0, title: '工業應用', sub:'Silicon按鍵',img:'/assets/imgs/industry/按鍵.jpg',content:'抗老化,耐磨,彈性佳'},
     {id:1, title: '工業應用', sub:'silicon O-Ring',img:'/assets/imgs/industry/o-ring.jpg',content:'耐酸鹼,耐溶劑,抗靜電'},
     {id:2, title: '工業應用', sub:'silicon防水圈',img:'/assets/imgs/industry/water-proof.jpg',content:'耐酸鹼,耐溶劑,抗靜電'},
-    {id:3, title: '醫療科技', sub:'silicon醫療矽膠',img:'/assets/imgs/Medical/silicon.jpg',content:'>無毒,無味,耐高溫'},
+    {id:3, title: '醫療科技', sub:'silicon醫療矽膠',img:'/assets/imgs/Medical/silicon.jpg',content:'無毒,無味,耐高溫'},
     {id:4, title: '生活科技', sub:'silicon醫療鍵盤',img:'/assets/imgs/life/鍵盤.jpg',content:'無毒,無味,防塵'},
     {id:5, title: '生活科技', sub:'silicon果凍套',img:'/assets/imgs/life/3C.jpg',content:'延展性佳,防水性佳,質地柔軟'},
     {id:6, title: '生活科技', sub:'silicon水上用品',img:'/assets/imgs/life/水上用品.jpg',content:'耐用,舒適,透光性佳'},
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -20,7 +20,7 @@ const navbar = () => {
     //     <Navbar style={style} variant="light" >
     //                 <Navbar.Brand as={Link} to="/">鈞翔實業有限公司</Navbar.Brand>
     //                 <Navbar.Toggle aria-controls="responsive-navbar-nav"  />
-    //                 <Nav className="ml-auto">
+    //                 <Nav classNameName="ml-auto">
     //                     <Nav.Link as={Link} to="/">Home</Nav.Link>
     //                     <Nav.Link as={Link} to="/services">Services</Nav.Link>
     //                     <NavDropdown title="Product" id="collasible-nav-dropdown">
@@ -36,46 +36,46 @@ const navbar = () => {
     //             </Navbar>
     const right=
     //id="navbarNavDropdown" for small size dropdown
-        <div class="collapse navbar-collapse navbar-header pl-100" id="navbarNavDropdown" >
-            <ul class="navbar-nav " >
-                <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <div className="collapse navbar-collapse navbar-header pl-100" id="navbarNavDropdown" >
+            <ul className="navbar-nav " >
+                <li className="nav-item active">
+                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="/services">Services</a>
+                <li className="nav-item">
+                <a className="nav-link" href="/services">Services</a>
                 </li>
                 
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     產品
                 </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="/product/industry">工業應用</a>
-                    <a class="dropdown-item" href="/product/medical">醫療科技</a>
-                    <a class="dropdown-item" href="/product/life">生活科技</a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item" href="/product/industry">工業應用</a>
+                    <a className="dropdown-item" href="/product/medical">醫療科技</a>
+                    <a className="dropdown-item" href="/product/life">生活科技</a>
                 </div>
                 </li>
 
             
 
-                <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
+                <li className="nav-item">
+                <a className="nav-link" href="/about">About</a>
                 </li>
 
-                <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact Us</a>
+                <li className="nav-item">
+                <a className="nav-link" href="/contact">Contact Us</a>
                 </li>
             </ul>
             </div>
     const header=
-        <nav class="navbar navbar-expand-md navbar-light bg-light ">
-        <div class="collapse navbar-collapse navbar-header pl-100"  >
-            <a class="navbar-brand" href="/">鈞翔實業</a>
+        <nav className="navbar navbar-expand-md navbar-light bg-light ">
+        <div className="collapse navbar-collapse navbar-header pl-100"  >
+            <a className="navbar-brand" href="/">鈞翔實業</a>
         </div>
         
-        <form class="form-inline my-2 my-lg-0">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <form className="form-inline my-2 my-lg-0">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
         </button>
 
         {right}
